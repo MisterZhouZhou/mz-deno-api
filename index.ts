50716fa36d99c3d458465889319773db56d3ce30
zhouwei <zhouwei1@cheyipai.com>
2023-06-01 14:37:36 +0800

    update

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
 import { serve } from "https://deno.land/std/http/server.ts";
 
-serve((_: Request) => new Response("Hello World"));
+serve((_: Request) => new Response("Hello World"), { port: 3000});
