9af0f09763dead9616f263f29e179320a35e156d
zhouwei <zhouwei1@cheyipai.com>
2023-06-04 00:03:38 +0800

    update

---
+++ mz-deno-fronted/dist/assets/index-4e8b91fe.js
@@ -0,0 +1,1 @@
+import{c as e,a as t}from"./index-37b51d30.js";const o=()=>e("h1",null,[t("home")]);export{o as default};
--- mz-deno-fronted/dist/assets/index-9e8e2915.js
+++
@@ -1,1 +0,0 @@
-import{c as e,a as t}from"./index-ab290778.js";const o=()=>e("h1",null,[t("home")]);export{o as default};
