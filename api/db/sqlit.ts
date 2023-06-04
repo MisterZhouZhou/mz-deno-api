a219f3f1020ec348174f2c301c91deb61bcd9c6e
MisterZhouZhou <min.li@juniorchina.com>
2023-06-04 22:48:37 +0800

    update

--- api/db/sqlit.ts
+++ api/db/sqlit.ts
@@ -1,6 +1,6 @@
 import { DB } from "$sqlite";
 
 // Open a database
-const sqlitDb = new DB(`${Deno.cwd()}/api/db/mz-deno-api.db`);
+const sqlitDb = new DB(`${Deno.cwd()}/api/db/mz-deno-api.db`, { mode: 'write'});
 
 export default sqlitDb
