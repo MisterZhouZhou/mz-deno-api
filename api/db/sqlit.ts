f770662208a20bac9439361794823645c18c49a2
zhouwei <zhouwei1@cheyipai.com>
2023-06-05 13:54:51 +0800

    update

--- api/db/sqlit.ts
+++ api/db/sqlit.ts
@@ -1,6 +1,6 @@
 import { DB } from "$sqlite";
 
 // Open a database
-const sqlitDb = new DB(`${Deno.cwd()}/api/db/mz-deno-api.db`, { mode: 'write'});
+const sqlitDb = new DB(`${Deno.cwd()}/api/db/mz-deno-api.db`, { mode: 'read'});
 
 export default sqlitDb
